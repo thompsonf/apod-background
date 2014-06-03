f5af17b04934a761471af59c07af9f34e5dee2c0
Frank Thompson <frankthompson@gmail.com>
2014-06-02 21:52:16 -0500

    properly size text box and allow multiline titles

--- apod.py
+++ apod.py
@@ -68,14 +68,20 @@ def resize_and_pad(img):
 	out_img.paste(img, (x_offset, y_offset))
 	return out_img
 
-def get_text_len(text, font):
+def get_text_width_and_height(text, font):
 	#is there a better way to do this? We shouldn't need to 
-	#create a new image just to figure out the width of text
+	#create a new image just to figure out the size of text
 	#in a certain font
 	im = Image.new("RGB",(0,0),"black")
 	draw = ImageDraw.Draw(im)
 	text_width, text_height = draw.textsize(text, font=font)
-	return text_width
+	return text_width, text_height
+
+def get_text_width(text, font):
+	return get_text_width_and_height(text, font)[0]
+
+def get_text_height(text, font):
+	return get_text_width_and_height(text, font)[1]
 
 def split_text_into_lines(text, width, font):
 	words = text.strip().split()
@@ -86,40 +92,66 @@ def split_text_into_lines(text, width, font):
 	lines = [words[0] + ' ']
 	words = words[1:]
 	for word in words:
-		if get_text_len(lines[-1] + word, font) <= width:
+		if get_text_width(lines[-1] + word, font) <= width:
 			#current word fits in current line
 			lines[-1] += word + ' '
 		else:
 			#current word does not fit in current line
-			#make a new line and add it there
+			#remove the space from the current line
+			lines[-1] = lines[-1][:-1]
+			#make a new line and add the word
 			lines.append(word + ' ')
 	return lines
 
 def add_title_and_explanation(img, title, explanation):
-	box_width = get_box_width()
-	box_height = 200
-	x = (RESX - box_width) // 2
-	y = EFFECTIVE_RESY - box_height
-	poly = Image.new('RGBA', (box_width, box_height), "black")
-	mask = Image.new(poly.mode, poly.size, (0,0,0,128))
-	img.paste(poly, (x, y), mask)
-
 	title_font = ImageFont.truetype("arial.ttf", 24)
 	exp_font = ImageFont.truetype("arial.ttf", 16)
+
+	box_width, x_margin, y_margin, space_under_title, line_spacing = get_box_info()
+	
+	#get size of title text
+	title_width = box_width - 2 * x_margin
+	title_lines = split_text_into_lines(title, box_width, title_font)
+	single_title_line_height = max([get_text_height(l, title_font) for l in title_lines])
+	title_height = single_title_line_height * len(title_lines) + line_spacing * (len(title_lines) - 1)
+
+	#get size of explanation text
+	text_width = box_width - 2 * x_margin
+	text_lines = split_text_into_lines(explanation, box_width, exp_font)
+	single_exp_line_height = max([get_text_height(l, exp_font) for l in text_lines])
+	#add  extra pixel of space between lines
+	text_height = single_exp_line_height * len(text_lines) + line_spacing * (len(text_lines) - 1)
+
+	box_height = 2 * y_margin + title_height + space_under_title + text_height
+
+	#box is centered horizontally
+	box_x = (RESX - box_width) // 2
+	#box is at the bottom of the page
+	box_y = EFFECTIVE_RESY - box_height
+
+	poly = Image.new('RGBA', (box_width, box_height), "black")
+	mask = Image.new(poly.mode, poly.size, (0,0,0,128))
+	img.paste(poly, (box_x, box_y), mask)
+
 	draw = ImageDraw.Draw(img)
 
+	#cur_y keeps track of the y-coord to draw the next line
+	cur_y = box_y + y_margin
 	#draw title
-	title_width, title_height = draw.textsize(title, font=title_font)
-	draw.text(((RESX - title_width) // 2, y + 5), title, (256,256,256), font=title_font)
+	for line in title_lines:
+		title_line_width = get_text_width(line, title_font)
+		draw.text(((RESX - title_line_width) // 2, cur_y), line, (256,256,256), font=title_font)
+		cur_y += single_title_line_height + line_spacing
+
+	#extra space of line_spacing was added. Remove that and add the space under title
+	cur_y = cur_y - line_spacing + space_under_title
 
 	#draw explanation
-	exp_x = x + 5
-	exp_y = y + 20 + title_height
-	lines = split_text_into_lines(explanation, box_width, exp_font)
-	for line in lines:
-		w, h = exp_font.getsize(line)
-		draw.text((exp_x, exp_y), line, (256, 256, 256), font=exp_font)
-		exp_y += h
+	exp_x = box_x + x_margin
+	for line in text_lines:
+		draw.text((exp_x, cur_y), line, (256, 256, 256), font=exp_font)
+		cur_y += single_exp_line_height + line_spacing
+
 	return img
 
 def prepare_image(old_fname, new_fname, title, explanation):
@@ -128,11 +160,15 @@ def prepare_image(old_fname, new_fname, title, explanation):
 	img = add_title_and_explanation(img, title, explanation)
 	img.save(new_fname)
 
-#get the max width of the text box containing the explanation
+#get various bits of info relating to the box
 #maybe make this smarter?
-def get_box_width():
-	#uses RESX and RESY set above
-	return RESX // 2
+def get_box_info():
+	width = RESX // 2
+	x_margin = 5
+	y_margin = 5
+	space_under_title = 20
+	line_spacing = 2
+	return width, x_margin, y_margin, space_under_title, line_spacing
 
 
 h = get_html()
